23b87e16d9934488335adaf39d2d99132b9c03ce
lucasangelino <angelino.n.lucas@gmail.com>
2023-03-01 10:33:53 -0300

    change hero component style

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -12,7 +12,7 @@ export const Hero = () => {
   };
 
   return (
-    <div className="px-4 py-40 mx-auto grid grid-cols-2 gap-2 max-w-6xl">
+    <div className="mt-12 mb-20 mx-auto grid grid-cols-1 md:grid-cols-2 gap-6 max-w-6xl">
       <Title />
       <Form onSubmit={handleSubmit} />
     </div>
@@ -20,24 +20,25 @@ export const Hero = () => {
 };
 
 const Title = () => (
-  <section className="flex flex-col">
-    <h1 className="text-8xl text-white font-bold my-4">OptiPic</h1>
-    <p className="my-8 text-xl text-slate-200">
+  <section className="flex flex-col gap-4">
+    <h1 className="text-7xl text-white font-bold">OptiPic</h1>
+    <p className="text-xl text-slate-300">
       Dale un{" "}
-      <span className="bg-blue-500 p-1 rounded-lg text-white">boost</span> a tu
-      web! Optipic te permite analizar cualquier sitio web en segundos. Con
-      reportes, código y mucho mas..{" "}
+      <span class="bg-gray-100 text-gray-800 text-lg font-medium px-2.5 py-0.5 rounded-full ">boost</span> a tu
+      web! Optipic te permite optimizar las imagenes de tu sitio en cuestion de segundos. Solo introduce tu URL y Optipic hará el resto. {" "
+      }
     </p>
-    <p className="text-xl text-slate-200">
-      Automatico, rápido y{" "}
-      <span className="bg-blue-500 p-1 rounded-lg text-white">gratis</span>
+    <p className="text-2xl font-semibold text-purple-400">
+      Automatico. {" "}
+      <span>Rápido.</span>{" "}
+      <span>Gratis.</span>
     </p>
   </section>
 );
 
 const Form = ({ onSubmit }) => (
-  <section className="flex flex-col justify-center items-center p-3">
-    <form action="" className="w-full" onSubmit={onSubmit}>
+  <section className="flex flex-col justify-center items-start gap-2 py-3">
+    <form action="" className="w-full flex flex-col gap-2" onSubmit={onSubmit}>
       <label htmlFor="urlInput" className="text-white text-2xl">
         Url de tu sitio
       </label>
@@ -48,16 +49,16 @@ const Form = ({ onSubmit }) => (
         className="w-full p-2 rounded-md bg-slate-200 text-slate-900"
       />
     </form>
-    <div className="flex flex-row justify-start items-center text-white">
-      <h3>¿No hay url? Prueba una de estas:</h3>
-      <ul className="flex flex-row justify-start items-center">
-        <li className="mx-2">
-          <a href="#" className="text-blue-700">
+    <div className="flex flex-col justify-start items-start text-white gap-4">
+      <h3 className="text-xs">¿No hay url? Prueba una de estas:</h3>
+      <ul className="flex flex-row justify-start items-center gap-4">
+        <li className="">
+          <a href="#" className="text-blue-700 bg-white p-2 rounded-md font-semibold">
             genbeta.com
           </a>
         </li>
-        <li className="mx-2">
-          <a href="#" className="text-blue-700">
+        <li className="">
+        <a href="#" className="text-blue-700 bg-white p-2 rounded-md font-semibold shadow-xl">
             infobae.com
           </a>
         </li>
